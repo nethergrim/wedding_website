d27802145f86e7e12a3412b20577cb60398b5aa7
Andrii Drobiazko <c2q9450@gmail.com>
2018-11-18 12:05:56 +0200

    Started integrating Grommet

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import WhatWeDo from './whatWeDo/whatWeDo'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -22,23 +23,14 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
+            { name: 'description', content: 'Weddings By Egupova' },
             { name: 'keywords', content: 'свадьба, егупова, евгения, организация свадеб Харьков' },
           ]}
         >
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        {/* <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div> */}
+        <WhatWeDo />
       </>
     )}
   />
---
+++ src/components/whatWeDo/whatWeDo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Text from 'grommet';
+
+
+class WhatWeDo extends React.Component {
+  render() {
+      return (
+          <Text>Hello</Text>
+      )
+  };
+
+
+}
+
+export default WhatWeDo;
